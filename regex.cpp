0aba78a8987a62f6cc30509d9f34c5f94028bd8b
abrartarafder <95165049+abrartarafder@users.noreply.github.com>
2022-12-22 21:32:06 -0500

    Create regex.cpp

---
+++ regex.cpp
@@ -0,0 +1,35 @@
+#include <iostream> 
+#include <regex> 
+#include<string.h> 
+using namespace std; 
+
+   
+int main() 
+{ 
+    //string
+    string phone_number; 
+   
+    // regex expression for pattern to be searched 
+//    ? refers to 0 or 1
+    regex regexp("^([+])?([0-9])?(-)?[(]?[0-9]{3}[)]?(-)?[0-9]{3}(-)?[0-9]{4}$"); 
+   
+    // matching variable (finds match within string)
+     smatch m; 
+
+
+    // prompt user
+    cout << "ENTER A PHONE NUMBER TO SEE IF ITS VALID: ";
+    cin >> phone_number;
+
+   
+    // regex_search that searches pattern regexp in the string mystr  
+    if (regex_search(phone_number, m, regexp) == true){
+  
+    cout<< "VALID PHONE NUMBER" << endl;
+
+    }
+    else{
+        cout<< "INVALID PHONE NUMBER" << endl;
+    }
+    return 0; 
+}
